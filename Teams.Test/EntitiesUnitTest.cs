120bfcdc975f89011777be3fb1eb5eb6cabd7df8
Chiayin <Chiayin@DESKTOP-IV3LRLM>
2024-12-12 16:02:00 -0500

    9. The solution contains a complete solution for a “Last Action Message” TagHelper - all that you need to do for this step is to make use of it in the layout.

--- Teams.Test/EntitiesUnitTest.cs
+++ Teams.Test/EntitiesUnitTest.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Teams.Entities;
 
 namespace Teams.Test
 {
     public class EntitiesUnitTest
     {
+        [Fact]
+        public void Player_WithValidData_PassesValidation()
+        {
+            // Arrange
+            var player = new Player
+            {
+                LeagueRegistrationNumber = "AB-12345678",
+                FirstName = "John",
+                LastName = "Doe",
+                Number = 99
+            };
+
+            var context = new ValidationContext(player);
+            var results = new List<ValidationResult>();
+
+            // Act
+            var isValid = Validator.TryValidateObject(player, context, results, true);
+
+            // Assert
+            Assert.True(isValid);
+            Assert.Empty(results);
+        }
     }
 }
